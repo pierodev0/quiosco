32e8e3ccbd83efaee09ba50f5381def60282ea56
Piero Bayona <pierodev21@gmail.com>
2023-10-07 22:08:21 -0500

    finalizar formulario de registro

--- react-quiosco/src/views/Registro.jsx
+++ react-quiosco/src/views/Registro.jsx
@@ -17,6 +17,52 @@ export default function Registro() {
               id='name'
               name='name'
               className='mt-2 block p-3 bg-gray-50 w-full'
+              placeholder='Ingresa tu nombre'
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='email'
+              className='text-slate-800'
+            >
+              Email:
+            </label>
+            <input
+              type='email'
+              id='email'
+              name='email'
+              className='mt-2 block p-3 bg-gray-50 w-full'
+              placeholder='Ingresa tu email'
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='password'
+              className='text-slate-800 capitalize'
+            >
+              password:
+            </label>
+            <input
+              type='password'
+              id='password'
+              name='password'
+              className='mt-2 block p-3 bg-gray-50 w-full'
+              placeholder='Ingresa tu password'
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              htmlFor='password_confirmation'
+              className='text-slate-800 capitalize'
+            >
+              repetir password:
+            </label>
+            <input
+              type='password'
+              id='password_confirmation'
+              name='password_confirmation'
+              className='mt-2 block p-3 bg-gray-50 w-full capitalize'
+              placeholder='repetir password'
             />
           </div>
         </form>

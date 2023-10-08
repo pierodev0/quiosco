a050a205515949d90ec341e1f1a884a36ed62a44
Piero Bayona <pierodev21@gmail.com>
2023-10-08 17:03:17 -0500

    mostrar imagen precio y nombre del producto

--- react-quiosco/src/components/Producto.jsx
+++ react-quiosco/src/components/Producto.jsx
@@ -1,6 +1,16 @@
-export default function Producto({producto}) {
-    console.log(producto);
+export default function Producto({ producto }) {
+  const { nombre, imagen, precio } = producto;
   return (
-    <div>Producto</div>
-  )
+    <div className='border p-3 shadow bg-white'>
+      <img
+        src={`/img/${imagen}.jpg`}
+        alt={`imagen ${nombre}`}
+        className='w-full'
+      />
+      <div className='p-5'>
+        <h3 className='text-2xl font-bold'>{nombre}</h3>
+        <p className='mt-5 font-black text-4xl text-amber-500'>{precio}</p>
+      </div>
+    </div>
+  );
 }
--- react-quiosco/src/views/Inicio.jsx
+++ react-quiosco/src/views/Inicio.jsx
@@ -6,7 +6,7 @@ export default function Inicio() {
     <>
       <h1 className='text-4xl font-black'>Inicio</h1>
       <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuacion</p>
-      <div className='grip gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
         {productos.map(producto => (
           <Producto key={producto.id} producto={producto}/>
         ))}

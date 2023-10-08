08997cf993a7180123f13acd9f863892b95a4f64
Piero Bayona <pierodev21@gmail.com>
2023-10-08 17:14:09 -0500

    finalizar mostrar productos

--- react-quiosco/src/components/Producto.jsx
+++ react-quiosco/src/components/Producto.jsx
@@ -1,5 +1,7 @@
+import { formatearDinero } from '../helpers';
 export default function Producto({ producto }) {
   const { nombre, imagen, precio } = producto;
+
   return (
     <div className='border p-3 shadow bg-white'>
       <img
@@ -8,8 +10,9 @@ export default function Producto({ producto }) {
         className='w-full'
       />
       <div className='p-5'>
-        <h3 className='text-2xl font-bold'>{nombre}</h3>
-        <p className='mt-5 font-black text-4xl text-amber-500'>{precio}</p>
+        <h3 className='text-2xl font-bold h-12'>{nombre}</h3>
+        <p className='mt-5 font-black text-4xl text-amber-500'>{formatearDinero(precio)}</p>
+        <button className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold'>Agregar</button>
       </div>
     </div>
   );

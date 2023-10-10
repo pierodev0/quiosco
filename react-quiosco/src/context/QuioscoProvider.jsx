a5069938736cf5a61b3429af6362a9a9d46571db
Piero Bayona <pierodev21@gmail.com>
2023-10-10 16:22:55 -0500

    feat: agrega modal para mostrar producto
    chore: instalar y configurar react-modal
    
    Filtrando el Producto a mostrar en el Modal
    
    Mostrando la Imagen del Producto en el Modal
    
    Mostrando el Resto de la información del Producto
    
    añade  funcionalidad de ajustar las cantidades

--- react-quiosco/src/context/QuioscoProvider.jsx
+++ react-quiosco/src/context/QuioscoProvider.jsx
@@ -1,20 +1,40 @@
 import { createContext, useState } from 'react';
 import { categorias as categoriasDB } from '../data/categorias';
+import { useEffect } from 'react';
 
 const QuiscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState(categoriasDB);
   const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
-
+  const [modal, setModal] = useState(false);
+  const [producto,setProducto] = useState({});
 
   const handleClickCategoria = (id) => {
-    const categoria = categorias.filter(categoria => categoria.id === id);
+    const categoria = categorias.filter((categoria) => categoria.id === id);
     setCategoriaActual(categoria[0]);
+  };
+
+  const handleClickModal = () => {
+    setModal(!modal);
+  };
+
+  const handleSetProducto = (producto) => {
+    setProducto(producto)
   }
 
   return (
-    <QuiscoContext.Provider value={{ categorias, categoriaActual,handleClickCategoria }}>
+    <QuiscoContext.Provider
+      value={{
+        categorias,
+        categoriaActual,
+        handleClickCategoria,
+        modal,
+        handleClickModal,
+        producto,
+        handleSetProducto
+      }}
+    >
       {children}
     </QuiscoContext.Provider>
   );
--- react-quiosco/src/views/Inicio.jsx
+++ react-quiosco/src/views/Inicio.jsx
@@ -12,7 +12,7 @@ export default function Inicio() {
     <>
       <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
       <p className='text-2xl my-10'>Elige y personaliza tu pedido a continuacion</p>
-      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+      <div className='grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
         {productos.map(producto => (
           <Producto key={producto.id} producto={producto}/>
         ))}

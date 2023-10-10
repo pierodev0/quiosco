a4964110c900f805f39f54e27391029dcc47dbd6
Piero Bayona <pierodev21@gmail.com>
2023-10-10 16:32:52 -0500

    chore: instalar y configurar react-modal

--- react-quiosco/src/layouts/Layout.jsx
+++ react-quiosco/src/layouts/Layout.jsx
@@ -1,18 +1,42 @@
 import { Outlet } from 'react-router-dom';
+import Modal from 'react-modal';
 import Sidebar from '../components/Sidebar';
 import Resumen from '../components/Resumen';
 import useQuiosco from '../hooks/useQuiosco';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
 export default function Layout() {
-  const {modal} =  useQuiosco();
-  console.log(modal)
+  const { modal, handleClickModal } = useQuiosco();
+  console.log(modal);
   return (
-    <div className='md:flex'>
-      <Sidebar />
-      <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
-        <Outlet />
-      </main>
-      <Resumen />
-    </div>
+    <>
+      <div className='md:flex'>
+        <Sidebar />
+        <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
+          <Outlet />
+        </main>
+        <Resumen />
+      </div>
+
+      {modal && (
+        <Modal
+          isOpen={modal}
+          style={customStyles}
+        >
+          <button onClick={handleClickModal}>Cerrar modal</button>
+          <p>Modal</p>
+        </Modal>
+      )}
+    </>
   );
 }
